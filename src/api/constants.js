c9b9f23b166bb1b4f1148c50cf7067983ee4bcff
Damika Gamlath <damika97.10@gmail.com>
2021-05-30 16:43:15 +0530

    test dep config

--- src/api/constants.js
+++ src/api/constants.js
@@ -3,7 +3,7 @@ export const IS_PROD = process.env.NODE_ENV === 'production';
 
 export const SERVER_URL = IS_PROD
   ? // In production we want to redirect to /whatever
-    ``
+    `http://spectrum.gq`
   : // In development we gotta redirect to localhost:3001/whatever tho
     'http://localhost:3001';
 
